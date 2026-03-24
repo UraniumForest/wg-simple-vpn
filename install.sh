49369a351a2bcff02901a43fa8c16c8cbd5cd4b1
UraniumForest <137532965+UraniumForest@users.noreply.github.com>
2026-03-24 14:55:20 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 umask 077
 set -e
 set -u
+
 green='\033[0;32m'
 bold='\033[1m'
 nc='\033[0m'
@@ -11,7 +12,7 @@ echo -e "${green}${bold}=== Starting wg-simple-vpn Installation ===${nc}"
 
 # Root check
 if [[ "$EUID" -ne 0 ]]; then
-    echo "Error: This script must be run with sudo."
+    echo "\nError: This script must be run with sudo."
     exit 1
 fi
 
@@ -21,14 +22,14 @@ apt update
 apt install -y wireguard qrencode iptables
 
 # Directory Structure
-echo -e "\n${bold}3. Creating configuration directories...${nc}"
+echo -e "\n${bold}2. Creating configuration directories...${nc}"
 mkdir -p /etc/wireguard/clients
 
-# 5. Command Integration
-echo -e "\n${bold}4. Installing the management script...${nc}"
+# Command Integration
+echo -e "\n${bold}3. Installing the management script...${nc}"
 # Assuming your main script is in the same folder as this installer
 cp wg-simple-vpn /usr/local/bin/wg-simple-vpn
 chmod +x /usr/local/bin/wg-simple-vpn
 
 echo -e "\n${green}${bold}=== Installation Completed Successfully ===${nc}"
-echo -e "You can now use the command: ${bold}wg-simple-vpn${nc}"
+echo -e "\nYou can now use the command: ${bold}wg-simple-vpn${nc}"
